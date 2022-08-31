3cfe41fc63468ab562947e87398132c350702129
AidanHimley <ah8uqq@virgina.edu>
2022-08-30 23:01:54 -0400

    finished my part, I think

--- ctutorial/list.c
+++ ctutorial/list.c
@@ -4,14 +4,26 @@
 #include <string.h>
 
 #include "list.h"
+#define BUF_SIZE 40
+
+void list_item_print (void *v) {
+    list_item_t *item = (list_item_t *) v;
+    printf("%s", (char *)(item->datum));
+}
+
+int datum_strcmp (const void *key, const void *with) {
+    return strcmp((const char *)key, (const char *)with);
+}
 
 // Ctor for list
-void list_init(list_t *l, int (*compare)(const void *key, const void *with), void (*datum_delete)(void *datum)) {
+void list_init (list_t *l, int (*compare)(const void *key, const void *with), void (*datum_delete)(void *datum)) {
     // Dummy head and tail nodes for doubly linked list
     list_item_t* head = malloc(sizeof(list_item_t));
     list_item_t* tail = malloc(sizeof(list_item_t));
     head->next = tail;
+    head->pred = NULL;
     tail->pred = head;
+    tail->next = NULL;
 
     l->head = head;
     l->tail = tail;
@@ -20,27 +32,70 @@ void list_init(list_t *l, int (*compare)(const void *key, const void *with), voi
     l->datum_delete = datum_delete;
 }
 
+// Dtor for list
+void list_delete (list_t *l) {
+    while (l->length > 0) {
+        list_remove_head(l);
+    }
+    free(l->head);
+    free(l->tail);
+}
+
+void list_visit_items (list_t *l, void (*visitor)(void *v)) {
+    list_item_t *curr = l->head->next;
+    while (curr != l->tail) {
+        visitor(curr);
+        curr = curr->next;
+    }
+}
+
+void list_insert_tail (list_t *l, void *v) {
+    list_item_t *new = malloc(sizeof(list_item_t));
+    new->datum = malloc(BUF_SIZE * sizeof(char));
+    strcpy(new->datum, v);
+    new->pred = l->tail->pred;
+    new->next = l->tail;
+    l->tail->pred->next = new;
+    l->tail->pred = new;
+    l->length++;
+}
+
+void list_remove_head (list_t *l) {
+    if (l->length > 0) {
+        list_item_t *del = l->head->next;
+        l->head->next = del->next;
+        del->next->pred = l->head;
+        l->datum_delete(del->datum);
+        free(del);
+        l->length--;
+    }
+}
+
 int main (int argc, char *argv[]) {
     // Verify that two command line arguments are passed
     if (argc != 3) {
-        printf("Wrong number of command line parameters supplied\n");
-        exit(-1);
+        printf("Wrong number of command line parameters supplied\nUsage: list_harness <input filename> <instruction>\n");
+        exit(1);
     }
 
     // Open file
     FILE* f = fopen(argv[1], "r");
     if (f == NULL) {
         printf("Error opening file %s\n", argv[1]);
+        exit(1);
     }
 
+    // Initialize list
+    list_t l;
+    list_init(&l, datum_strcmp, free);
+
     // Read by line until EOF
-    char buffer[40]; char trimmed[40];
-    while (fgets(buffer, 40, f) != NULL) {
+    char buffer[BUF_SIZE]; char trimmed[BUF_SIZE];
+    while (fgets(buffer, BUF_SIZE, f) != NULL) {
         int i;
         int j = 0;
-
         // Remove all whitespace
-        for (i = 0; i < 40; i++) {
+        for (i = 0; i < BUF_SIZE; i++) {
             if (!isspace(buffer[i]) || buffer[i] == '\n') {
                 trimmed[j] = buffer[i];
                 j++;
@@ -48,15 +103,29 @@ int main (int argc, char *argv[]) {
         }
         if (strcmp(argv[2], "echo") == 0) {
             // Echo trimmed line
-            printf(trimmed);
+            printf("%s", trimmed);
         } else if (strcmp(argv[2], "tail") == 0) {
-            // TODO
+            // add line to list
+            list_insert_tail(&l, trimmed);
         } else if (strcmp(argv[2], "tail-remove") == 0) {
-            // TODO
+            // add line to list
+            list_insert_tail(&l, trimmed);
         } else {
             printf("Unknown command supplied");
-            exit(-1);
+            exit(1);
         }
-
     }
+    fclose(f);
+
+    if (strcmp(argv[2], "tail") == 0) {
+        list_visit_items(&l, list_item_print);
+    } else if (strcmp(argv[2], "tail-remove") == 0) {
+        while (l.length > 0) {
+            list_remove_head(&l); list_remove_head(&l); list_remove_head(&l);
+            list_visit_items(&l, list_item_print);
+            printf("----------------------------------\n");
+        }
+    }
+    list_delete(&l);
+    return 0;
 }
--- ctutorial/list.h
+++ ctutorial/list.h
@@ -13,7 +13,11 @@ typedef struct list {
     void (*datum_delete)(void *);
 } list_t;
 
+void list_item_print(void *v);
+int datum_strcmp(const void *key, const void *with);
+
 void list_init(list_t *l, int (*compare)(const void *key, const void *with), void (*datum_delete)(void *datum));
+void list_delete(list_t *l);
 void list_visit_items(list_t *l, void (*visitor)(void *v));
 void list_insert_tail(list_t *l, void *v);
 void list_remove_head(list_t *l);
