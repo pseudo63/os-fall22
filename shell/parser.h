fb36c3ce283c266392f7ecd30e7a2432cb18af3d
pseudo63 <jalong063@gmail.com>
2022-09-06 23:02:51 -0400

    All

---
+++ shell/parser.h
@@ -0,0 +1,11 @@
+#ifndef PARSER_H_
+#define PARSER_H_
+
+
+#include <sstream>
+#include <string.h>
+#include <vector>
+
+std::vector <std::string> parse_from_line(const std::string &line);
+
+#endif
