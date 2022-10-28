b8a650343a9ed30f6e8b66da0594b9d98fe97d3d
pseudo63 <jalong063@gmail.com>
2022-10-28 15:06:15 -0400

    Notes on skeleton

--- thread-pool/pool-skeleton/pool/pool.cc
+++ thread-pool/pool-skeleton/pool/pool.cc
@@ -1,19 +1,43 @@
 #include "pool.h"
 
 Task::Task() {
+    // Initialize condition member variable
+    // Other metadata?
 }
 
 Task::~Task() {
+    //cleanup condition
 }
 
 ThreadPool::ThreadPool(int num_threads) {
+    // Creates and joins a number of threads equal to num_threads
+    // Initialize threads with the worker function described below
 }
 
 void ThreadPool::SubmitTask(const std::string &name, Task* task) {
+    
+    // When a task is submitted, add it to the task queue
+    // Make sure the queue mutex is used to protect the task queue
+    // Also add the task into a map with key name and value task
+    // After unlocking the queue mutex, signal that a task is available
+}
+
+void* WorkerFunction(void* args) {
+    
+    // Run in a superloop because threads should work until stop is called
+    // Add a conditional wait within superloop, checking if task is available
+    // Ensure mutual exclusion on task queue. Make sure lock is released before calling run
+    // Dequeue a task and call that task's run function
+    
 }
 
 void ThreadPool::WaitForTask(const std::string &name) {
+    // Find the task using the key name to search in map
+    // waits on the per task condition variable
 }
 
 void ThreadPool::Stop() {
+    // cleanup threads
 }
+
+// TODO: cleanup queue and mutex
--- thread-pool/pool-skeleton/pool/pool.h
+++ thread-pool/pool-skeleton/pool/pool.h
@@ -4,6 +4,8 @@
 
 class Task {
 public:
+    // Each task will need a completion condition variable to use in the wait function
+
     Task();
     virtual ~Task();
 
@@ -12,6 +14,9 @@ public:
 
 class ThreadPool {
 public:
+
+    // Add member variables for taskQueue mutex and task queue
+
     ThreadPool(int num_threads);
 
     // Submit a task with a particular name.
@@ -19,6 +24,9 @@ public:
  
     // Wait for a task by name, if it hasn't been waited for yet. Only returns after the task is completed.
     void WaitForTask(const std::string &name);
+
+    // Worker function for thread initialization
+    void* WorkerFunction(void* args);
 
     // Stop all threads. All tasks must have been waited for before calling this.
     // You may assume that SubmitTask() is not caled after this is called.
